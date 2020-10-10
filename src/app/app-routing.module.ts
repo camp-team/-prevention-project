399cec1019cd822004d3be9410ee1548cb897c2d
Hideki Kamakura <kakihk695@gmail.com>
2020-10-10 15:06:12 +0900

    feat(app-routing):guard実装#21

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+import { GuestGuard } from './guards/guest.guard';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
@@ -7,25 +9,35 @@ const routes: Routes = [
     path: '',
     pathMatch: 'full',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+    canLoad: [AuthGuard],
+    canActivate: [AuthGuard],
   },
   {
     path: 'init-form',
     loadChildren: () =>
       import('./init-form/init-form.module').then((m) => m.InitFormModule),
+    canLoad: [AuthGuard],
+    canActivate: [AuthGuard],
   },
   {
     path: 'quize',
     loadChildren: () =>
       import('./quize/quize.module').then((m) => m.QuizeModule),
+    canLoad: [AuthGuard],
+    canActivate: [AuthGuard],
   },
   {
     path: 'settings',
     loadChildren: () =>
       import('./settings/settings.module').then((m) => m.SettingsModule),
+    canLoad: [AuthGuard],
+    canActivate: [AuthGuard],
   },
   {
     path: 'welcome',
     component: WelcomeComponent,
+    canLoad: [GuestGuard],
+    canActivate: [GuestGuard],
   },
 ];
 
