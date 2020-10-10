7d66a33edfc3eff45463fec0f45dd8978819177e
peyo-code <mr233425@gmail.com>
2020-10-10 12:45:34 +0900

    feat(pokemon-list): ユーザー情報取得

--- src/app/services/pokemon-list.service.ts
+++ src/app/services/pokemon-list.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { User } from 'firebase';
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonListService {
+  uid: string;
+  user$: Observable<User> = this.afAuth.authState.pipe(
+    switchMap((afUser) => {
+      if (afUser) {
+        this.uid = afUser && afUser.uid;
+        return this.db.doc<User>(`user/${afUser.uid}`).valueChanges();
+      } else {
+        return of(null);
+      }
+    })
+  );
 
-  constructor() { }
+  constructor(
+    private afAuth: AngularFireAuth,
+    private db: AngularFirestore
+  ) { }
 }
