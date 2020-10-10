e248ad4ba448037259a6d2103cb8100a7f7b506b
Hideki Kamakura <kakihk695@gmail.com>
2020-10-10 14:49:09 +0900

    feat(user.function.ts):ポケモンコレクション作成#9

--- functions/src/user.function.ts
+++ functions/src/user.function.ts
@@ -1,11 +1,17 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+
 export const db = admin.firestore();
 
 export const createUserAccount = functions
   .region('asia-northeast1')
   .auth.user()
   .onCreate(async (user) => {
+    let pokemons = {};
+    for (let i = 1; i <= 151; i++) {
+      const pokemon = { [i]: false };
+      pokemons = Object.assign(pokemon, pokemons);
+    }
     db.doc(`users/${user.uid}`)
       .set({
         uid: user.uid,
@@ -13,7 +19,9 @@ export const createUserAccount = functions
         createdAt: new Date(),
       })
       .then(() => {
-        functions.logger.info('success');
+        return db
+          .doc(`users/${user.uid}/collections/pokemons`)
+          .set({ pokemons });
       })
       .catch((err) => {
         functions.logger.error(err);
