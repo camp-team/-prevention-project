43699a367dd456adf24d6ac20e8c5d4558d64090
kokiinoue45 <koki_inoue45@icloud.com>
2020-10-10 23:01:14 +0900

    質問画面マークアップ

--- src/app/quize/quize/quize.component.ts
+++ src/app/quize/quize/quize.component.ts
@@ -6,10 +6,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quize.component.scss']
 })
 export class QuizeComponent implements OnInit {
+  answers = [false, false, false];
+
+  questions: {
+    label: string;
+    questionText: string;
+    state: string;
+    yes: string;
+    no: string;
+    back: string;
+  }[] = [
+      {
+        label: 'しつもん 1',
+        questionText: '手あらい、うがいはしましたか？',
+        state: 'one',
+        yes: 'はい',
+        no: 'いいえ',
+        back: 'もどる'
+      },
+      {
+        label: 'しつもん 2',
+        questionText: 'マスクはつけておでかけしましたか？',
+        state: 'two',
+        yes: 'はい',
+        no: 'いいえ',
+        back: 'もどる'
+      },
+      {
+        label: 'しつもん 3',
+        questionText: '人とはなれてせいかつしましたか？',
+        state: 'three',
+        yes: 'はい',
+        no: 'いいえ',
+        back: 'もどる'
+      },
+    ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  answerTrue(index: number) {
+    this.answers[index] = true;
+  }
+
+  answerFalse(index: number) {
+    this.answers[index] = false;
+  }
 }
