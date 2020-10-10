1f0009cb5ae94009f7b1aa200ebbf9b7a8b53480
Hideki Kamakura <kakihk695@gmail.com>
2020-10-10 15:25:47 +0900

    feat(home.module):full-calenar インポート#13

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
 import { CalendarComponent } from './calendar/calendar.component';
+import { FullCalendarModule } from '@fullcalendar/angular';
 
 @NgModule({
   declarations: [HomeComponent, CalendarComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, FullCalendarModule],
 })
 export class HomeModule {}
