ca86b7f99d1d11bc51b9f85b4d8ae2e84c9f5bbf
Hideki Kamakura <kakihk695@gmail.com>
2020-10-11 08:28:56 +0900

    feat(calendar.component.ts):カレンダーにポケモン表示#13

--- src/app/home/calendar/calendar.component.ts
+++ src/app/home/calendar/calendar.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin from '@fullcalendar/interaction';
+import { Subscription } from 'rxjs';
+import { UserLoginList } from 'src/app/interfaces/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss'],
 })
-export class CalendarComponent implements OnInit {
+export class CalendarComponent implements OnInit, OnDestroy {
+  private subscription = new Subscription();
+
   private readonly preCalendarOption = {
     plugins: [dayGridPlugin],
     initialView: 'dayGridMonth',
@@ -23,65 +28,41 @@ export class CalendarComponent implements OnInit {
     dayCellContent(event) {
       event.dayNumberText = event.dayNumberText.replace('日', '');
     },
-    dayCellClassNames(event) {
-      if (event.date < new Date(2018, 0, 1)) {
-        event.isFuture = true;
-      }
-    },
-    businessHours: false,
-    dateClick: this.handleDateClick.bind(this),
-  };
-  calendarOptions = {
-    plugins: [dayGridPlugin],
-    initialView: 'dayGridMonth',
-    weekends: true,
-    theme: 'standart',
-    headerToolbar: {
-      left: 'prevYear prev',
-      center: 'title',
-      right: 'next nextYear',
-    },
-    events: [],
-    locale: 'ja',
-    dayCellContent(event) {
-      event.dayNumberText = event.dayNumberText.replace('日', '');
-    },
-    // dayCellClassNames(event) {
-    //   if (event.date < new Date(2018, 0, 1)) {
-    //     event.isFuture = true;
-    //   }
-    // },
     businessHours: true,
-    // dateClick: this.handleDateClick.bind(this),
   };
+  calendarOptions = this.preCalendarOption;
 
-  constructor() {
-    const data = [];
-    data.push({
-      date: '2020-10-10',
-      className: 'fc-pokemon',
-    });
-    this.calendarOptions.events = data;
+  constructor(
+    private userService: UserService,
+    private authService: AuthService
+  ) {}
+
+  private getLoginList() {
+    this.subscription = this.userService
+      .getLoginList(this.authService.uid)
+      .subscribe((allData: UserLoginList[]) => {
+        this.calendarOptions = this.preCalendarOption;
+        const data = [];
+        allData.forEach((monthData: UserLoginList) => {
+          for (let i = 1; i <= 31; i++) {
+            const className: string = 'fc-pokemon' + monthData[i]?.pokemonId1;
+            if (monthData[i]) {
+              data.push({
+                date: monthData[i].date,
+                className,
+              });
+            }
+          }
+        });
+        this.calendarOptions.events = data;
+      });
   }
 
-  private handleDateClick(arg) {
-    // const week = this.weekList[arg.dayEl.cellIndex];
-    // this.date = arg.dateStr
-    //   .slice(2)
-    //   .replace(/-/g, '.')
-    //   .replace(/$/, '(' + week + ')');
-    // if (this.today >= this.date && this.minDate <= this.date) {
-    //   this.dailyInfoService.createDailyInfo({
-    //     authorId: this.authService.uid,
-    //     date: this.date,
-    //   });
-    //   this.router.navigate(['/daily-detail'], {
-    //     queryParams: {
-    //       date: this.date,
-    //     },
-    //   });
-    // }
+  ngOnInit(): void {
+    this.getLoginList();
   }
 
-  ngOnInit(): void {}
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
