04335673d3b981036035289f63ca6255e8413f30
Hideki Kamakura <kakihk695@gmail.com>
2020-10-10 23:39:21 +0900

    feat(user.service):カレンダー用ログインリスト取得関数作成#13

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -4,9 +4,10 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { User } from 'firebase';
 import { Observable, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
+import { UserLoginList } from '../interfaces/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
   uid: string;
@@ -21,24 +22,25 @@ export class UserService {
     })
   );
 
-  constructor(
-    private afAuth: AngularFireAuth,
-    private db: AngularFirestore
-  ) { }
+  constructor(private afAuth: AngularFireAuth, private db: AngularFirestore) {}
 
   getPokemonList(uid: string) {
+    return this.db.doc(`users/${uid}/collections/pokemons`).valueChanges();
+  }
+
+  getLoginList(uid: string): Observable<UserLoginList[]> {
     return this.db
-      .doc(`users/${uid}/collections/pokemons`)
+      .collection<UserLoginList>(`users/${uid}/dates`)
       .valueChanges();
   }
 
   updateUser(user: User): Promise<void> {
     return this.db.doc(`users/${user.uid}`).update(user);
   }
+
   updateMyPokemonCollections(pokemonId: number, uid: string) {
-    console.log('affaf', pokemonId);
     return this.db.doc(`users/${uid}/collections/pokemons`).update({
-      [pokemonId]: true
+      [pokemonId]: true,
     });
   }
 }
