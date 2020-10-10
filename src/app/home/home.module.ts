793803ffacab42422604ef3e59d53f244e9b8a7e
chamaboy <hiroponpoko07@gmail.com>
2020-10-10 15:38:36 +0900

    feat(home): grid表示 #6

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
+FullCalendarModule.registerPlugins([dayGridPlugin, interactionPlugin]);
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  imports: [CommonModule, HomeRoutingModule, FullCalendarModule],
 })
-export class HomeModule { }
+export class HomeModule {}
