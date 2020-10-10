a3997ca79e1dd25867ad55c4b4d73c1a8ce7373f
Hideki Kamakura <kakihk695@gmail.com>
2020-10-11 08:28:12 +0900

    fix(home.component.ts):カレンダーオプション削除#13

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CalendarOptions } from '@fullcalendar/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +6,6 @@ import { CalendarOptions } from '@fullcalendar/angular';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  calendarOptions: CalendarOptions = {
-    initialView: 'dayGridMonth',
-  };
-
   constructor() {}
 
   ngOnInit(): void {}
