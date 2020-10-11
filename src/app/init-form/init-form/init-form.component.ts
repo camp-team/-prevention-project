6f591d1da28929be4ad28e4d13458022a75f5488
kokiinoue45 <koki_inoue45@icloud.com>
2020-10-11 15:44:13 +0900

    swiper js 導入

--- src/app/init-form/init-form/init-form.component.ts
+++ src/app/init-form/init-form/init-form.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { Pokemon } from 'src/app/interfaces/pokemon';
 import { PokemonService } from 'src/app/services/pokemon.service';
+import { UserService } from 'src/app/services/user.service';
+import Swiper, { Navigation, Pagination } from 'swiper';
 
 @Component({
   selector: 'app-init-form',
   templateUrl: './init-form.component.html',
   styleUrls: ['./init-form.component.scss']
 })
-export class InitFormComponent implements OnInit {
+export class InitFormComponent implements OnInit, AfterViewInit {
+  active: number;
   pokemonList = this.pokemonService.pokemonList;
   initPokemons: Omit<Pokemon, 'type'>[] = [
     {
@@ -23,12 +28,22 @@ export class InitFormComponent implements OnInit {
       id: this.pokemonList[6].id
     }
   ];
+
+  swiper;
+  getingPokemonId: number;
   constructor(
-    private pokemonService: PokemonService
+    private pokemonService: PokemonService,
+    private userService: UserService,
+    private snackBar: MatSnackBar,
+    private route: Router
   ) { }
 
-  ngOnInit(): void {
-    const swiper = new Swiper('.swiper-container', {
+  ngOnInit(): void { }
+
+  ngAfterViewInit(): void {
+    Swiper.use([Navigation, Pagination]);
+    this.swiper = new Swiper('.swiper-container', {
+      init: true,
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
@@ -41,14 +56,28 @@ export class InitFormComponent implements OnInit {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
-      coverflow: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true
-      },
+
       loop: false
+    });
+  }
+
+  test() {
+    const index = this.swiper.activeIndex;
+    if (index === 0) {
+      console.log('1');
+      this.getingPokemonId = 1;
+    } else if (index === 1) {
+      console.log('4');
+      this.getingPokemonId = 4;
+    } else {
+      console.log('7');
+      this.getingPokemonId = 7;
+    }
+    this.userService.updateMyPokemonCollections(this.getingPokemonId, this.userService.uid).then(() => {
+      this.snackBar.open('おめでとう！' + this.pokemonList[this.getingPokemonId - 1].name + 'をゲットしたぞ 🥳', null, {
+        duration: 2500
+      });
+      this.route.navigateByUrl('/');
     });
   }
 }
