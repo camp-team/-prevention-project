c45a2be4a43c6d1b652b2b98add393a3ea471b62
Hideki Kamakura <kakihk695@gmail.com>
2020-10-11 14:51:12 +0900

    feat(home.component.ts):今日のクイズしたか判定#52

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,12 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserLoginList } from 'src/app/interfaces/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  constructor() {}
+export class HomeComponent implements OnInit, OnDestroy {
+  private subscription = new Subscription();
 
-  ngOnInit(): void {}
+  isTodayAnswer = false;
+
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+    private datePipe: DatePipe
+  ) {}
+
+  private transDate() {
+    return this.datePipe.transform(new Date(), 'yyyy-MM-dd');
+  }
+
+  getTodayLogin() {
+    const date: string = this.transDate();
+    const day: number = Number(date.substr(8, 2));
+    this.userService
+      .getTodayLogin(this.authService.uid)
+      .subscribe((res: UserLoginList) => {
+        this.isTodayAnswer = res[day]?.date ? true : false;
+      });
+  }
+
+  ngOnInit(): void {
+    this.getTodayLogin();
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
