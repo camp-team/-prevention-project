2eb84fc73ce6609906be297f8d049801130bd6fd
kokiinoue45 <koki_inoue45@icloud.com>
2020-10-11 12:32:15 +0900

    swiper js導入

--- src/app/init-form/init-form/init-form.component.ts
+++ src/app/init-form/init-form/init-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Pokemon } from 'src/app/interfaces/pokemon';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-init-form',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./init-form.component.scss']
 })
 export class InitFormComponent implements OnInit {
-
-  constructor() { }
+  pokemonList = this.pokemonService.pokemonList;
+  initPokemons: Omit<Pokemon, 'type'>[] = [
+    {
+      name: this.pokemonList[0].name,
+      id: this.pokemonList[0].id
+    },
+    {
+      name: this.pokemonList[3].name,
+      id: this.pokemonList[3].id
+    },
+    {
+      name: this.pokemonList[6].name,
+      id: this.pokemonList[6].id
+    }
+  ];
+  constructor(
+    private pokemonService: PokemonService
+  ) { }
 
   ngOnInit(): void {
+    const swiper = new Swiper('.swiper-container', {
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      }, effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      initialSlide: 1,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      coverflow: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true
+      },
+      loop: false
+    });
   }
-
 }
